4ea9c7855cad9242d3797ff7e3f8fdab69d6acd5
Qazaq159 <zat.tyme.su@gmail.com>
2024-06-08 04:47:11 +0500

    added cors to settings

--- blogapp/settings/base.py
+++ blogapp/settings/base.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'storages',
     'django_summernote'
 ]
@@ -66,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'blogapp.urls'
