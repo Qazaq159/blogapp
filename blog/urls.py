36dc5d0176f56ca632f94aa9eb5a402340114d8f
Nursat Sakyshev <98874935+NursatSakyshev@users.noreply.github.com>
2024-10-25 00:45:17 +0500

    Feature/about us (#4)
    * about page
    
    * adding bootstrap
    
    * adding bootstrap

--- blog/urls.py
+++ blog/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('search/', views.SearchListView.as_view(), name='search'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('summernote/', include('django_summernote.urls'))
+    path('summernote/', include('django_summernote.urls')),
+    path('about/', views.about, name='about'),
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
