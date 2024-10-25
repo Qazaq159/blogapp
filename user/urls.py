d5cce3c40fe0e1c5ac38ea67666cb5c2428009bf
Didar775 <d_alikhan@kbtu.kz>
2024-10-25 10:43:16 +0500

    added user profile

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_user),
+    path('profile/', views.profile, name='profile'),
+    path('profile/<str:username>/', views.profile_detail, name='profile_detail')
 ]
